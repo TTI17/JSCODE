c3bbf3e7c129d5ce9be8aa44d5be855f4721a118
lizard <muhammadievalbert171099@gmail.com>
2025-10-04 11:35:14 +0400

    calculation

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert()
+function getVariables(){
+    let m = Number(document.loandata.m.value);
+    let h = Number(document.loandata.h.value);
+    const g = 9.8;
+
+    let e = m*g*h;
+    let space_out = document.getElementById("space");
+    console.log(e)
+    space_out.innerHTML = e.toFixed(2);
+}
